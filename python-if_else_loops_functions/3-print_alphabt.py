2c1d61e9947ce491423b4077190c8d9cfcd0f9e4
Myriam Hajjaji <myriamhajjaji@Myriams-MacBook-Pro.local>
2025-04-24 15:40:10 +0100

    task 3

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for letter in range(97, 123):
-    if chr(letter) is not '101' and chr(letter) is not '113':
+    if chr(letter) != '101' and chr(letter) != '113':
         print("{}".format(chr(letter)), end="")
