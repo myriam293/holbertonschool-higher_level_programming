2fa7d0ec7b9a0a0ef7c6770f8f99b83a5146339d
Myriam Hajjaji <myriamhajjaji@Myriams-MacBook-Pro.local>
2025-05-09 15:34:55 +0100

    task 1

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python3
+"""This module defines a Square class."""
+
+
 class Square:
+    """Represents a square."""
+
     def __init__(self, size):
+        """Initialize a new Square instance.
+
+        Args:
+            size (int): The size of the square.
+        """
         self.__size = size
