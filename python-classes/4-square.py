315087779d7615fabf270165178a645e79c4744e
Myriam Hajjaji <myriamhajjaji@Myriams-MacBook-Pro.local>
2025-05-11 20:25:27 +0100

    task 4

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-"""This module defines a Square class with size validation and area calculation."""
+"""This module defines a Square class.
+
+It includes size validation and area calculation.
+"""
 
 
 class Square:
