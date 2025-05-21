19b1813642f1686fe0135764af118a5944e8b3ff
Myriam Hajjaji <myriamhajjaji@myriams-mbp.home>
2025-05-21 16:02:21 +0200

    task 5

--- python-more_classes/5-rectangle.py
+++ python-more_classes/5-rectangle.py
@@ -51,7 +51,8 @@ class Rectangle:
             return (0)
         return ((self.__width * 2) + (self.__height * 2))
 
-    def __str__(self):
+
+def __str__(self):
         """Return the printable representation of the Rectangle.
 
         Represents the rectangle with the # character.
@@ -72,3 +73,6 @@ class Rectangle:
         rect += ", " + str(self.__height) + ")"
         return (rect)
 
+    def __del__(self):
+        """Print a message for every deletion of a Rectangle."""
+        print("Bye rectangle...")
