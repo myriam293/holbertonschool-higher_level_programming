0c9f623f25e5922fcb2be69104a0a05cc1758da2
Myriam Hajjaji <myriamhajjaji@myriams-mbp.home>
2025-05-23 17:52:24 +0200

    task 7

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -2,6 +2,13 @@
 """Defines the Rectangle class."""
 
 
+class Rectangle:
+    """Represent a rectangle.
+
+    Attributes:
+        number_of_instances (int): The number of Rectangle instances.
+        print_symbol (any): The symbol used for string representation.
+    """
 
     number_of_instances = 0
     print_symbol = "#"
