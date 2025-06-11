1476e0cbc66ea70e4b98ce5f7121463fed5886e0
Myriam Hajjaji <myriamhajjaji@Myriams-MacBook-Pro.local>
2025-06-11 17:02:18 +0200

    task 7

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -6,8 +6,7 @@ from the specified class ; otherwise False."""
 
 def inherits_from(obj, a_class):
     """Checks if the object is an instance of
-    a class that inherited (directly or indirectly)
-    from the specified class
+    a class that inherited from the specified class
 
     Args:
         obj (any): The object to check.
