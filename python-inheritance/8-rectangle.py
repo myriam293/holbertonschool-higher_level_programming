75e150f6f3778f4338742bc9055fda03b5070470
Myriam Hajjaji <myriamhajjaji@Myriams-MacBook-Pro.local>
2025-06-13 15:08:49 +0200

    task 8

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""Write a class Rectangle that inherits from BaseGeometry (7-base_geometry.py)."""
+"""Write a class Rectangle that inherits
+from BaseGeometry (7-base_geometry.py)."""
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
