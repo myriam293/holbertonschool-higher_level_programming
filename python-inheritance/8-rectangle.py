5bea3ce75f544dce9ad82ea17a1917fc640b6d98
Myriam Hajjaji <myriamhajjaji@Myriams-MacBook-Pro.local>
2025-06-13 15:25:30 +0200

    task 8

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,8 +1,29 @@
 #!/usr/bin/python3
 """Write a class Rectangle that inherits
 from BaseGeometry (7-base_geometry.py)."""
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
+
+class BaseGeometry:
+    """Reprsent base geometry."""
+
+    def area(self):
+        """Not yet implemented."""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validate a parameter as an integer.
+
+        Args:
+            name (str): The name of the parameter.
+            value (int): The parameter to validate.
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is <= 0.
+        """
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
 
 class Rectangle(BaseGeometry):
     """Represent a rectangle using BaseGeometry."""
