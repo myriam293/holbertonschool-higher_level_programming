01b0c8fe455122499e3df6f8d05f2fd2e38ae024
Myriam Hajjaji <myriamhajjaji@Myriams-MacBook-Pro.local>
2025-06-19 17:25:03 +0200

    task 0

---
+++ python-abc/task_00_abc.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Module containing Animal class and its inheritances"""
+from abc import ABC, abstractmethod
+
+
+class Animal(ABC):
+    """The Animal class"""
+    @abstractmethod
+    def sound(self):
+        """Method to make sound"""
+        pass
+
+
+class Dog(Animal):
+    """Dog class Inherited from Animal"""
+    def sound(self):
+        """Dog goes Bark"""
+        return "Bark"
