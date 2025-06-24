45f6bbbb9471fbd4d566e55a277e0ad134268fc1
Myriam Hajjaji <myriamhajjaji@Myriams-MacBook-Pro.local>
2025-06-24 19:43:11 +0200

    task 6

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Write a function that creates an Object from a “JSON file”."""
+import json
+
+
+def load_from_json_file(filename):
+    """Create an Object from a JSON file."""
+    with open(filename, 'r', encoding='UTF-8') as file:
+        return json.loads(file.read())
