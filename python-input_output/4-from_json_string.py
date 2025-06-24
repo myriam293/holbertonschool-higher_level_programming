568cc31f44efe96834af3047c050132596ac108e
Myriam Hajjaji <myriamhajjaji@Myriams-MacBook-Pro.local>
2025-06-24 12:24:12 +0200

    task 4

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -5,5 +5,6 @@ import json
 
 
 def from_json_string(my_str):
-    """Return an object (Python data structure) represented by a JSON string."""
+    """Return an object (Python data structure)
+    represented by a JSON string."""
     return json.loads(my_str)
