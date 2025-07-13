adcc014029930009179054fbd6e07b8490df271a
Myriam Hajjaji <myriamhajjaji@Myriams-MacBook-Pro.local>
2025-07-14 00:37:34 +0100

    task 8

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """Return the dictionary description with simple
 data structure for JSON serialization."""
+
+
 def class_to_json(obj):
     """Return the dictionary description for
     JSON serialization of an object."""
