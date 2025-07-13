be87259983798f4056b7008b23035ded006afd90
Myriam Hajjaji <myriamhajjaji@Myriams-MacBook-Pro.local>
2025-07-14 00:50:49 +0100

    task 10

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -1,23 +1,17 @@
 #!/usr/bin/python3
-"""Define a Student class with a method
-to retrieve a dictionary representation."""
+"""Write a class Student that defines a student
+by public instance attributes."""
 
 
 class Student:
-    """Defines a student by first_name, last_name, and age."""
+    """Defines a student by: first_name, last_name, age."""
 
     def __init__(self, first_name, last_name, age):
-        """Instantiate with first_name, last_name, and age."""
+        """Instantiation with first_name, last_name and age."""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-    def to_json(self, attrs=None):
-        """
-        Retrieve a dictionary representation of a Student instance.
-        If attrs is a list of strings, only those attributes are
-        retrieved. Otherwise, all attributes are retrieved.
-        """
-        if isinstance(attrs, list) and all(type(attr) is str for attr in attrs):
-            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+    def to_json(self):
+        """Retrieves a dictionary representation of a Student instance."""
         return self.__dict__
