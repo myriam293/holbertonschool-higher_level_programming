0fd23de2d429f6e6b7243b5b0906374f21f16360
Myriam Hajjaji <myriamhajjaji@Myriams-MacBook-Pro.local>
2025-07-15 10:54:57 +0100

    task 10

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -18,6 +18,7 @@ class Student:
         If attrs is a list of strings, only those attributes are
         retrieved. Otherwise, all attributes are retrieved.
         """
-        if isinstance(attrs, list) and all(type(attr) is str for attr in attrs):
+        if (isinstance(attrs, list)
+                and all(type(attr) is str for attr in attrs)):
             return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
         return self.__dict__
