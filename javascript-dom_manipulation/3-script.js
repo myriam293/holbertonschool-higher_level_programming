2a6a4f721e6512c82f0bcdef6158371e99a7ceda
Myriam Hajjaji <myriamhajjaji@Myriams-MacBook-Pro.local>
2025-08-10 18:07:29 +0100

    task 3

--- javascript-dom_manipulation/3-script.js
+++ javascript-dom_manipulation/3-script.js
@@ -1,5 +1,12 @@
-if (headColor.classList.value.includes('green')) {
-    headColor.classList.replace('green', 'red');
-} else {
-    headColor.classList.replace('red', 'green');
-}
+const headColor = document.querySelector('header');
+
+const setColor = document.querySelector('#toggle_header');
+setColor.addEventListener('click', () => {
+    if (headColor.classList.length === 0) {
+        headColor.classList.add('green');
+    } else if (headColor.classList.value.includes('green')) {
+        headColor.classList.replace('green', 'red');
+    } else if (headColor.classList.value.includes('red')) {
+        headColor.classList.replace('red', 'green');
+    }
+});
