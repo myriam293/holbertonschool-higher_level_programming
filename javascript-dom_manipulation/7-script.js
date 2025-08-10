4089cfb75083828abc6d113494776ba0ec9ff242
Myriam Hajjaji <myriamhajjaji@Myriams-MacBook-Pro.local>
2025-08-10 19:01:01 +0100

    task 8

---
+++ javascript-dom_manipulation/7-script.js
@@ -0,0 +1,11 @@
+const FilmList = document.querySelector('#list_movies');
+
+fetch('https://swapi-api.hbtn.io/api/films/?format=json')
+    .then((response) => response.json())
+    .then((data) => {
+        for (let index = 0; index < data.results.length; index++) {
+            const NewFilm = document.createElement('li');
+            NewFilm.textContent = data.results[index].title;
+            FilmList.appendChild(NewFilm);
+        }
+    });
--- javascript-dom_manipulation/8-script.js
+++ javascript-dom_manipulation/8-script.js
@@ -1,11 +1,8 @@
-const FilmList = document.querySelector('#list_movies');
-
-fetch('https://hellosalut.stefanbohacek.dev/?lang=fr')
-    .then((response) => response.json())
-    .then((data) => {
-        for (let index = 0; index < data.results.length; index++) {
-            const NewFilm = document.createElement('li');
-            NewFilm.textContent = data.results[index].title;
-            FilmList.appendChild(NewFilm);
-        }
-    });
+document.addEventListener('DOMContentLoaded', () => {
+    fetch('https://hellosalut.stefanbohacek.dev/?lang=fr')
+        .then((response) => response.json())
+        .then((data) => {
+            const helloContent = document.getElementById('hello');
+            helloContent.textContent = data.hello;
+        });
+})
