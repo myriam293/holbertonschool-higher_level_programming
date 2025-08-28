0ad7245db70d49b5af4bd2986949ae32354dd69b
Myriam Hajjaji <myriamhajjaji@Myriams-MacBook-Pro.local>
2025-08-28 14:28:13 +0100

    task 7

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,4 @@
+-- A script that creates the database hbtn_0d_usa and the table cities (in the database hbtn_0d_usa) on your MySQL server.
+CREATE DATABASE if NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE if NOT EXISTS cities (id INT AUTO_INCREMENT UNIQUE NOT NULL PRIMARY KEY,state_id INT NOT NULL,name VARCHAR(256) NOT NULL,FOREIGN KEY (state_id) REFERENCES states (id));
