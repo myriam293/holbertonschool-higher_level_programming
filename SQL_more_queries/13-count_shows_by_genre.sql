9308a7bf4fb908e61ede4c6496160ec1139149a0
Myriam Hajjaji <myriamhajjaji@Myriams-MacBook-Pro.local>
2025-08-29 10:36:59 +0100

    task 13

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -2,7 +2,7 @@
 SELECT g.name AS genre,
 COUNT(m.show_id) AS number_of_shows
 FROM tv_genres AS g
-INNER JOIN tv_show_genres
+INNER JOIN tv_show_genres AS m
 ON g.id = m.genre_id
 GROUP BY g.name
 ORDER BY number_of_shows DESC;
