3c0bcc0ebeb359d9a2c9098b849aa582a3f6099f
Myriam Hajjaji <myriamhajjaji@Myriams-MacBook-Pro.local>
2025-09-01 11:30:34 +0100

    task 0

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,24 +1,22 @@
 #!/usr/bin/python3
-"""Module for selecting all states from the database."""
+"""Lists all states from the database hbtn_0e_0_usa"""
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     import MySQLdb
     from sys import argv
 
     db = MySQLdb.connect(
-        host='localhost',
+        host="localhost",
         port=3306,
         user=argv[1],
         passwd=argv[2],
         db=argv[3]
     )
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
 
-    cursor = db.cursor()
-
-    cursor.execute('SELECT * FROM states ORDER BY id ASC')
-
-    for state in cursor.fetchall():
+    for state in cur.fetchall():
         print(state)
 
-    cursor.close()
+    cur.close()
     db.close()
