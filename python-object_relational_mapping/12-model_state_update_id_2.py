3354f161d843f92b27c861a7ceff09642d5bc6e4
Myriam Hajjaji <myriamhajjaji@Myriams-MacBook-Pro.local>
2025-09-02 14:40:09 +0100

    task 12

--- python-object_relational_mapping/12-model_state_update_id_2.py
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -7,22 +7,28 @@ from model_city import Base, City
 from model_state import State
 from sys import argv
 
+# Run only when executed
 if __name__ == "__main__":
 
+    # Engine creation with mysql and mysqldb DBAPI
     engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
                            .format(argv[1], argv[2], argv[3]))
 
+    # Creating all classes in DB
     Base.metadata.create_all(engine)
 
+    # Creating Session and its instance
     Session = sessionmaker(bind=engine)
     session = Session()
 
+    # The Query
     query = (
         session.query(State, City)
         .join(City, State.id == City.state_id)
         .order_by(City.id)
         .all())
 
+    # Printing the result
     for state, city in query:
         print("{}: ({}) {}".format(state.name, city.id, city.name))
 
